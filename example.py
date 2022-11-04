4678dc59e68c7ad32af22f14b1ad8cca0ad09d29
0xMihalich <bayan-mobile@list.ru>
2022-11-04 22:40:47 +1000

    Add files via upload

---
+++ example.py
@@ -0,0 +1,28 @@
+# импорт библиотеки
+from cramfs import cramFS
+
+filename = "example.img" # указываем необходимый образ/диск/байты или объект BytesIO
+offset = 512 # указываем смещение, по которому в файле находится образ cramfs (необязательно)
+size = 64622592 # указываем размер образа (необязательно)
+
+# работа с библиотекой
+test = cramFS(filename, offset, size)
+
+# получаем список объектов в корневой папке
+files = test.ls()
+for file in files:
+    print(file)
+
+test.cd('lib') # переходим в папку lib
+
+print(test.pwd()) # убеждаемся, что мы находимся в папке lib
+print(test.read('libcharset.so.1.0.0')) # читаем файл libcharset.so.1.0.0 из папки lib
+
+print(test.cd('alsa-lib')) # переходим в папку alsa-lib внутри папки lib
+
+# получаем список объектов в папке alsa-lib
+files = test.ls()
+for file in files:
+    print(file)
+
+test.cd('/') # возвращаемся в корневую папку
