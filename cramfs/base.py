2e27a3ee45b2b41cbf7f8a0a74eecae08d633cdf
0xMihalich <bayan-mobile@list.ru>
2022-11-04 22:02:37 +1000

    добавлен декоратор

--- cramfs/base.py
+++ cramfs/base.py
@@ -1,11 +1,13 @@
-class base:
+import abc
+
+class base(metaclass=abc.ABCMeta):
     def __init__(self, _io, _parent=None, _root=None):
         self._io = _io
         self._parent = _parent
         self._root = _root if _root else self
         self._read()
 
+    @abc.abstractmethod
     def _read(self):
         # абстрактная функция, задается в каждом классе
-        pass
-
+        return
